e2a0ef3277b80955b92474ee3a5703e764d446d9
Walter Ralls <ralls.walter@gmail.com>
2025-04-29 12:39:51 -0500

    Works well enough

--- test.py
+++ test.py
@@ -5,7 +5,11 @@ import threading
 import game
 import level_loader
 import ast
+import pygame
 
+clock = pygame.time.Clock
+desired_ticks = 6
+is_pressing = False
 df = pd.read_csv('paths.csv')
 best_row = df.loc[df['reward'].idxmax()]
 print(best_row)
@@ -29,7 +33,15 @@ game_thread = threading.Thread(target=game.game_loop, args=(start_state, level))
 game_thread.daemon = True
 game_thread.start()
 
-for key in pattern:
-    keyboard.press(key)
-    time.sleep(0.1)
-    keyboard.release(key)
+for key in pattern:  
+    if not is_pressing:
+        keyboard.press(key)
+        is_pressing = True
+        press_ticks = 0
+    elif is_pressing:
+        press_ticks += 1
+        if press_ticks >= desired_ticks:
+            keyboard.release(key)
+            is_pressing = False
+
+    clock.tick(60)
